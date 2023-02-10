96260508a3892551bdd536bd9dbbd7f677977649
PolyNoodle94 <emidollar@hotmail.com>
2023-02-10 10:51:06 -0600

    second commit. Project + Bonus Done

--- index.js
+++ index.js
@@ -1,26 +1,30 @@
-const { readFile } = require('fs').promises;
+const { readFile } = require("fs").promises;
 
 const mostFrequentWord = (text) => {
   const words = text.toLowerCase().match(/[^_\W]+/g);
   const tally = {};
   let mostFrequentWord = null;
-  
-  words.forEach(word => {
-    tally[word] = (tally[word] || 0) + 1 ;
-    if(!tally[mostFrequentWord] || tally[word] > tally[mostFrequentWord])
+
+  words.forEach((word) => {
+    tally[word] = (tally[word] || 0) + 1;
+    if (!tally[mostFrequentWord] || tally[word] > tally[mostFrequentWord])
       mostFrequentWord = word;
   });
   return mostFrequentWord;
-}
+};
 
 const findPassword = async () => {
-  // fs.readFile(poems/starting-poem.txt)
-  const poem1 = await readFile("poems/starting-poem.txt", "utf-8");
-  const poem2Name = mostFrequentWord(poem1)
-  const poem2 = await readFile(`poems/${poem2Name}.txt`, "utf-8")
-  const poem3Name = mostFrequentWord(poem2);
-  const poem3 = await readFile(`poems/${poem3Name}.txt`, "utf-8")
-  console.log(`Password: ` + mostFrequentWord(poem3));
-}
+  try {
+    const poem1 = await readFile("poems/starting-poem.txt", "utf-8");
+    const poem2Name = mostFrequentWord(poem1);
+    const poem2 = await readFile(`poems/${poem2Name}.txt`, "utf-8");
+    const poem3Name = mostFrequentWord(poem2);
+    const poem3 = await readFile(`poems/${poem3Name}.txt`, "utf-8");
+    console.log(poem3);
+    console.log(`Password: ` + mostFrequentWord(poem3));
+  } catch (error) {
+    console.log("YOU SUCKK!!!!");
+  }
+};
 
 findPassword();
